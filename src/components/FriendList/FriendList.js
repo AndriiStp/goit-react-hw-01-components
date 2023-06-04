0cf13ef20a2603d759e1575956b7237cd7c2fd45
Andrii Stepanenko <andrii.stp@gmail.com>
2023-06-04 22:27:50 +0300

    2nd

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,9 +1,23 @@
-const FriendList = ({ avatar, name, isOnline }) => {
+import PropTypes from 'prop-types';
+import FriendListItem from 'components/FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
   return (
     <>
-      <ul class="friend-list"></ul>
+      <ul className={css.friend__list}>
+        {friends.map(friend => (
+          <FriendListItem key={friend.id} {...friend} />
+        ))}
+      </ul>
     </>
   );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.number.isRequired })
+  ),
 };
 
 export default FriendList;
--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -1,12 +1,13 @@
-const FriendListItem = () => {
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
+
+const FriendListItem = ({ id, avatar, name, isOnline }) => {
   return (
-    <>
-      <li class="item">
-        <span class="status"></span>
-        <img class="avatar" src="" alt="User avatar" width="48" />
-        <p class="name"></p>
-      </li>
-    </>
+    <li className={css.item} key={id}>
+      <span className="status"></span>
+      <img className={css.avatar} src={avatar} alt={name} width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
   );
 };
 
